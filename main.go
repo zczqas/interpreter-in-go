a5ff7dcc63bb707229598578024b206d0a6201f1
Infamous-Ironman <128158627+Infamous-Ironman@users.noreply.github.com>
2025-02-13 17:29:24 +0545

    feat: repl (read, eval, print, loop)

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"go_inter/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Komi Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
