38fb355161044719837112e18013e03da992e956
Infamous-Ironman <128158627+Infamous-Ironman@users.noreply.github.com>
2025-02-16 21:48:05 +0545

    feat: parser error basic handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,25 @@
 package parser
 
 import (
+	"fmt"
 	"go_inter/ast"
 	"go_inter/lexer"
 	"go_inter/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -84,6 +89,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
