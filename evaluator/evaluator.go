3397ff6e930e1a54ad863813192225920d987cc4
Infamous-Ironman <128158627+Infamous-Ironman@users.noreply.github.com>
2025-02-25 09:36:56 +0545

    feat: String concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,6 +15,7 @@ var (
 const (
 	IDENTIFIER_NOT_FOUND = "identifier not found"
 	NOT_FUNC_ERROR       = "not a function"
+	UNKNOEN_OPERATOR     = "unknown operator"
 )
 
 func Eval(node ast.Node, env *object.Environment) object.Object {
@@ -207,6 +208,8 @@ func evalInfixExpression(
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
@@ -323,4 +326,17 @@ func unwrapReturnValue(obj object.Object) object.Object {
 	}
 
 	return obj
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("%s: %s %s %s", UNKNOEN_OPERATOR, left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
