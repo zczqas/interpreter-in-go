b73f68b31698c0c318ab58e0319382b21cfc36a1
Infamous-Ironman <128158627+Infamous-Ironman@users.noreply.github.com>
2025-02-25 19:05:44 +0545

    feat: parsing array literal and indexes with unit tests

--- parser/parser.go
+++ parser/parser.go
@@ -14,9 +14,10 @@ const (
 	EQUALS      // ==
 	LESSGREATER // > or <
 	SUM         // +
-	PRODUCT     //*
+	PRODUCT     // *
 	PREFIX      // -X or !X
 	CALL        // myFunction(X)
+	INDEX       // array[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -29,6 +30,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type Parser struct {
@@ -76,6 +78,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	return p
 }
@@ -415,30 +418,6 @@ func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	return exp
 }
 
-// func (p *Parser) parseCallArguments() []ast.Expression {
-// 	args := []ast.Expression{}
-
-// 	if p.peekTokenIs(token.RPAREN) {
-// 		p.nextToken()
-// 		return args
-// 	}
-
-// 	p.nextToken()
-// 	args = append(args, p.parseExpression(LOWEST))
-
-// 	for p.peekTokenIs(token.COMMA) {
-// 		p.nextToken()
-// 		p.nextToken()
-// 		args = append(args, p.parseExpression(LOWEST))
-// 	}
-
-// 	if !p.expectPeek(token.RPAREN) {
-// 		return nil
-// 	}
-
-// 	return args
-// }
-
 func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 	list := []ast.Expression{}
 
@@ -473,4 +452,17 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 	array.Elements = p.parseExpressionList(token.RBRACKET)
 
 	return array
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
+	return exp
 }
