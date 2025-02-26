8f70c0b64ddcfd2bfcbc5b4eea44447310c1c40e
Infamous-Ironman <128158627+Infamous-Ironman@users.noreply.github.com>
2025-02-26 20:32:35 +0545

    fix: formatting fix

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -106,7 +106,7 @@ var builtins = map[string]*object.Builtin{
 	"puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
-				fmt.Printf(arg.Inspect())
+				fmt.Println(arg.Inspect())
 			}
 
 			return NULL
